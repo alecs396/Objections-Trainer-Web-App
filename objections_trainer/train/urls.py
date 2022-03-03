5bc9b49f7e3c6ecf526bbd8a9b41f740e554b198
alecs396 <a.swainston16@gmail.com>
2022-03-02 20:38:22 -0700

    added add_objections

--- objections_trainer/train/urls.py
+++ objections_trainer/train/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='trainer'),
+    path('add_objection/', views.add_objection, name='add_objection'),
 ]
--- objections_trainer/train/views.py
+++ objections_trainer/train/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Trainer</h1>')
+
+def add_objection(request):
+    return HttpResponse('<h1>Create New Objection</h1>')
